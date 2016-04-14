33d9f5e98f9d00e48a66aeec63f1e096135e6f5e
Jakob Hok <dat15jh1@varg-8.student.lth.se>
2016-04-14 17:08:34 +0200

    added RStudio with plotResult script + two input files

---
+++ RStudio/plotResult.R
@@ -0,0 +1,25 @@
+# function  for  plotting  data
+plotresult = function(file , start = 1) {
+  data  <- read.csv(file)
+  data  <- data[start:nrow(data) ,]
+  # mean <- sum(data)/(nrow(data))
+  plot(data , type = 'l')
+  # y <- rep.int(mean, nrow(data))
+  # lines(data, y, col="green")
+  
+}
+
+inputFil <- "Random_2000"
+sort <- "bubbleSort"
+iter <- "600"
+terminal <- paste("java Algorithms ../input/", inputFil, " ", sort, " ", iter, sep="")
+output <- paste("output/", paste(sort, inputFil, iter, sep="_"), sep="")
+
+setwd("/h/d9/j/dat15jh1/github/EDAA35_projekt/bin/")
+system(terminal)
+setwd("/h/d9/j/dat15jh1/github/EDAA35_projekt")
+
+plotresult(output, start=1) # plot to  screent
+pdf(paste(output,".pdf",sep=""))
+plotresult(output, start=1) # plot to pdf  file
+dev.off()
